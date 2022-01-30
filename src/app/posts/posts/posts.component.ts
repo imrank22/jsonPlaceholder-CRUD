271d9ec9d72d5305f050116aba39bfeff75d6a45
Imran Khan <kimran0009@gmail.com>
2022-01-30 22:18:47 +0530

    added fetch post button and directly fetch post if user is not new

--- src/app/posts/posts/posts.component.ts
+++ src/app/posts/posts/posts.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Post } from 'src/app/interfaces/posts.interface';
 import { CommonService } from 'src/app/services/common.service';
 
@@ -9,57 +11,72 @@ import { CommonService } from 'src/app/services/common.service';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent implements OnInit, OnDestroy {
 
-  posts: Post[] = []
-  search: string = ''
-  isUpdating: boolean = false
-  selectedPostId: number = 0
-  isSortingAsc: boolean = true
+  subscriptions$ : Subscription[] = []
+  posts:           Post[] = []
+  search:          string = ''
+  isUpdating:      boolean = false
+  selectedPostId:  number = 0
+  isSortingAsc:    boolean = true
+  isPostsLoaded:   boolean = false
+  showLoader:      boolean = false
+  
   addPost = this._fb.group({
     title: [ '', [ Validators.required, Validators.pattern(/^[a-zA-Z0-9 ]*$/), Validators.minLength(5), Validators.maxLength(30) ] ],
     body: [ '', [ Validators.required, Validators.pattern(/^[a-zA-Z0-9 ]*$/), Validators.minLength(10), Validators.maxLength(100) ] ],
     userId: [ '', [ Validators.required, Validators.pattern(/^[0-9]*$/) ] ],
   })
 
-  constructor( private postService: CommonService, private _fb: FormBuilder, private _snackbar: MatSnackBar ) { }
+  constructor( private postService: CommonService, private _fb: FormBuilder, private _snackbar: MatSnackBar, private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
-    this.fetchPosts()
+    // directly fetch posts from the server, if user is back from comments or any other page
+    let subscription = this.route.queryParams.subscribe(params =>{
+      if( params['isPostsFetched'] ){
+        this.fetchPosts()
+      }
+    });
+    this.subscriptions$.push(subscription)
   }
 
   get userId(){ return this.addPost.get('userId') }
   get title(){ return this.addPost.get('title') }
   get body(){ return this.addPost.get('body') }
-
+  
   fetchPosts(){
-    this.postService.getListOfPosts$().subscribe((data: Post[])=>{
+    this.showLoader = true
+    let subscription = this.postService.getListOfPosts$().subscribe((data: Post[])=>{
       this.posts = data.splice(0, 4)
       this.posts.map(post =>{
-
+        
         //fetching number of comments for each post
-        this.postService.getCommentsForPost$(post.id).subscribe(res=>{
+        let subscription1 = this.postService.getCommentsForPost$(post.id).subscribe(res=>{
           post.commentLength = res.length
-        })
+        });
+        this.subscriptions$.push(subscription1)
         return post
-      })
-      console.log(this.posts)
-    })
+      });
+      this.showLoader = false
+      this.isPostsLoaded = true
+    });
+    this.subscriptions$.push(subscription)
   }
-
+  
   handleDelete(postId: number){
     this.posts = this.posts.filter(post => post.id !== postId)
   }
-
+  
   handleSearch(){
-    this.postService.getListOfPosts$().subscribe((data: Post[])=> this.posts = data.splice(0,4).filter(post => post.title.includes(this.search.toLowerCase())))
+    let subscription = this.postService.getListOfPosts$().subscribe((data: Post[])=> this.posts = data.splice(0,4).filter(post => post.title.includes(this.search.toLowerCase())));
+    this.subscriptions$.push(subscription)
   }
-
+  
   handleSort(){
     this.isSortingAsc = !this.isSortingAsc
     this.posts.reverse()
   }
-
+  
   handleUpdate(post: Post){
     this.isUpdating = true
     this.selectedPostId = post.id
@@ -69,12 +86,12 @@ export class PostsComponent implements OnInit {
       userId: post.userId
     })
   }
-
+  
   resetUpdate(){
     this.isUpdating = false
     this.addPost.reset()
   }
-
+  
   handleAddPost(){
     let payload = {
       id: this.isUpdating ? this.selectedPostId : Math.floor(Math.random() * 100),
@@ -98,4 +115,9 @@ export class PostsComponent implements OnInit {
     this._snackbar.open(message, 'OK', { duration: 3000 })
     this.isUpdating = false
   }
+  
+  ngOnDestroy(): void {
+    this.subscriptions$.forEach(subscription => subscription.unsubscribe())
+  }
+
 }
